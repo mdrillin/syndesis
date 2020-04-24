31c94d44acbeec2d9735262b3a208c524fcd1986
Mark Drilling <mdrillin@redhat.com>
2020-04-24 13:15:19 -0500

    Revert "dv-ls-integration rebase"
    This reverts commit 7586f227c413aca550253b2ce94dbd5053f52b29.

--- app/ui-react/packages/api/src/index.ts
+++ app/ui-react/packages/api/src/index.ts
@@ -68,4 +68,6 @@ export { useApiConnectorCreator } from './useApiConnectorCreator';
 export { ICreateConnectorProps } from './useApiConnectorCreator';
 export { useApiProviderSpecification } from './useApiProviderSpecification';
 export { useApiProviderIntegration } from './useApiProviderIntegration';
-export { useConnectorCredentialsConnect } from './useConnectorCredentialsConnect';
+export {
+  useConnectorCredentialsConnect,
+} from './useConnectorCredentialsConnect';
